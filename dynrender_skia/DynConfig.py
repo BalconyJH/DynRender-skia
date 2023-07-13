f269f998015adf7976731592edcb2eecbdb31cc2
Polyisoprene <lzxder@outlook.com>
2023-07-13 14:42:11 +0800

    修改了一些东西

--- dynrender_skia/DynConfig.py
+++ dynrender_skia/DynConfig.py
@@ -1,16 +1,27 @@
-from loguru import logger
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   DynConfig.py
+@Time    :   2023/07/13 14:20:02
+@Author  :   Polyisoprene 
+@Version :   1.0
+@Desc    :   None
+"""
+
+
 from os import path, getcwd
+from loguru import logger
 from typing import Optional
 from zipfile import ZipFile
 
 
 class MakeStaticFile:
     def __init__(self, data_path: Optional[str] = None) -> None:
-
         self.data_path: str = data_path
         self.check_cache_file()
 
     def check_cache_file(self) -> None:
+        """查询缓存文件是否存在"""
         # 查询是否有data_path参数
         # 没有的话静态文件目录就设置在程序的运行目录
         if self.data_path is None:
@@ -23,12 +34,11 @@ class MakeStaticFile:
             # 如果不存在静态目录将自带的压缩文件解压过去
             if not path.exists(static_path):
                 logger.info("未检测到static目录")
-                logger.info("用户未传入data路径,将在程序运行目录创建static目录")
-                logger.info("创建static目录中...")
-                file = ZipFile(path.join(current_dir, "Static.zip"))
+                file = self.unzip_file(
+                    "用户未传入data路径,将在程序运行目录创建static目录", "创建static目录中...", current_dir
+                )
                 file.extractall(program_running_path)
                 logger.info("static目录创建成功")
-        # 有data_path参数参数
         else:
             # 确认当前运行的文件所在的目录
             current_dir = path.dirname(path.abspath(__file__))
@@ -38,8 +48,13 @@ class MakeStaticFile:
                 static_path = path.join(self.data_path, "Static")
                 # 如果静态文件的目录不存在就直接解压文件
                 if not path.exists(static_path):
-                    logger.info("未检测到static目录")
-                    logger.info("使用用户传入路径创建static目录中...")
-                    file = ZipFile(path.join(current_dir, "Static.zip"))
+                    file = self.unzip_file(
+                        "未检测到static目录", "使用用户传入路径创建static目录中...", current_dir
+                    )
                     file.extractall(self.data_path)
                     logger.info("static目录创建成功")
+
+    def unzip_file(self, arg0, arg1, current_dir):
+        logger.info(arg0)
+        logger.info(arg1)
+        return ZipFile(path.join(current_dir, "Static.zip"))

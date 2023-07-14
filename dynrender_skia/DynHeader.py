850ebf968a59594f7cb7b2dea0eb1856fc94e2f1
Polyisoprene <lzxder@outlook.com>
2023-07-14 17:26:50 +0800

    完善了图片头的渲染

--- dynrender_skia/DynHeader.py
+++ dynrender_skia/DynHeader.py
@@ -1,7 +1,9 @@
 import asyncio
 from os import path
+from time import localtime, strftime, time
 from typing import Optional
 
+import httpx
 import numpy as np
 import skia
 from dynamicadaptor.Header import Head
@@ -10,33 +12,102 @@ from DynStyle import PolyStyle
 
 
 class BiliHeader:
-    def __init__(self,static_path:str,style:PolyStyle,header_message) -> None:
-        self.face_path = path.join(static_path,"Cache","Face")
-        self.emoji_path = path.join(static_path,"Cache","Emoji")
-        self.pendant_path = path.join(static_path,"Cache","Pendant")
-        self.src_path = path.join(static_path,"Src")
+    def __init__(self, static_path: str, style: PolyStyle, header_message) -> None:
+        self.face_path = path.join(static_path, "Cache", "Face")
+        self.emoji_path = path.join(static_path, "Cache", "Emoji")
+        self.pendant_path = path.join(static_path, "Cache", "Pendant")
+        self.src_path = path.join(static_path, "Src")
         self.style = style
         self.canvas = None
         self.message = None
-    
-    async def run(self,header_message:Head) -> Optional[np.ndarray]:
+
+    async def run(self, header_message: Head) -> Optional[np.ndarray]:
         try:
             self.message = header_message
-            surface = skia.Surface(1080,400)
+            surface = skia.Surface(1080, 400)
             self.canvas = surface.getCanvas()
             self.canvas.clear(skia.Color(*self.style.color.backgroud.normal))
-            
+            await asyncio.gather(self.paste_logo(),
+                                 self.draw_name(),
+                                 self.draw_pub_time())
+
         except Exception:
             logger.exception("Error")
             return None
+    async def past_face(self):
+        pass
+    
+    
+    async def get_face_and_pendant(self,img_type:bool=True):
+        if img_type:
+            img_name = f"{self.message.mid}.webp"
+
+            img_url = f"{self.message.face}@240w_240h_1c_1s.webp"
+            img_path = path.join(self.face_path, img_name)
+        else:
+            img_name = f"{self.message.pendant.pid}.png"
+            img_url = f"{self.message.pendant.image}@360w_360h.webp"
+            img_path = path.join(self.pendant_path, img_name)
+        if path.exists(img_path) and (not img_type or time() - int(path.getmtime(img_path)) <= 43200):
+            return skia.Image.open(img_path)
+        img = await self.get_pictures(img_url)
+        if img is not None:
+            img.save(img_path)
+            return img
+        return None
+    
+    async def get_pictures(self,url):
+        try:
+            async with httpx.AsyncClient() as client:
+                resp = client.get(url)
+                assert resp.status_code == 200
+            return skia.Image.MakeFromEncoded(resp.content).convert(alphaType=skia.AlphaType.kPremul_AlphaType)
+        except Exception:
+            return None
+    
+    async def circle_face(self,img,size):
+        surface = skia.Surface(img.dimensions().width(),img.dimensions().height())
+        canvas = surface.getCanvas()
+        paint = skia.Paint(
+            Style=skia.Paint.kFill_Style,
+            Color = skia.Color(255,255,255,255),
+            AntiAlias=True)
+        radius = int(img.dimensions().width()/2)
+        canvas.drawCircle(radius,radius,radius,paint)
+        paint = skia.Paint(
+        Style=skia.Paint.kStroke_Style,
+        StrokeWidth=5,
+        Color = skia.Color(251, 114, 153),
+        AntiAlias=True)
+        image_array =  np.bitwise_and(img.toarray(skia.ColorType.kRGBA_8888_ColorType),canvas.toarray())
+        surface = skia.Surface(image_array)
+        with surface as canvas:
+            canvas.drawCircle(radius,radius,radius-2,paint)
+        return skia.Image.fromarray(canvas.toarray()).resize(size,size)
+
+    async def draw_pub_time(self):
+        if self.message.pub_time:
+            pub_time = self.message.pub_time
+        elif self.message.pub_ts:
+            pub_time = strftime(
+                "%Y-%m-%d %H:%M:%S", localtime(self.message.pub_ts))
+        else:
+            pub_time = time.strftime(
+                "%Y-%m-%d %H:%M:%S", localtime(time()))
+        
+        await self.draw_text(pub_time,
+                             self.style.font.font_family,
+                             self.style.font.font_style,
+                             self.style.color.font_color.sub_title,
+                             self.style.font.font_size.time,
+                             200, 320)    
     
     async def paste_logo(self) -> None:
-        
-        logo = skia.Image.open(path.join(self.src_path, "bilibili.png")).convert(alphaType=skia.kUnpremul_AlphaType).resize(231, 105)
-        
-        await self.paste(self.canvas,logo,(433, 20))
-    
-    
+
+        logo = skia.Image.open(path.join(self.src_path, "bilibili.png")).convert(
+            alphaType=skia.AlphaType.kPremul_AlphaType).resize(231, 105)
+        await self.paste(self.canvas, logo, (433, 20))
+
     async def draw_name(self):
         # 如果是大会员的话
         if self.message.vip and self.message.vip.status == 1:
@@ -48,13 +119,38 @@ class BiliHeader:
                 color = self.style.color.font_color.name_small_vip
         else:
             color = self.style.color.font_color.text
-        skia_color = skia.Color(*color)
-        for i in self.message.name:
-            pass
-    
-    async def paste(self,canvas,image,position):
-        x,y = position
+        await self.draw_text(self.message.name,
+                             self.style.font.font_family,
+                             self.style.font.font_style,
+                             skia.Color(*color),
+                             self.style.font.font_size.name,
+                             200, 250)
+
+    async def draw_text(self, text, font_family, font_style, font_color, font_size, x, y):
+        paint = skia.Paint(AntiAlias=True, Color=font_color)
+        font_name = font_family
+        offset = x
+        for i in text:
+            if typeface := skia.FontMgr().matchFamilyStyleCharacter(
+                font_family,
+                font_style,
+                ["zh", "en"],
+                ord(i),
+            ):
+                text_family_name = typeface.getFamilyName()
+                if font_name != text_family_name:
+                    font_name = text_family_name
+                    font = skia.Font(typeface, font_size)
+                else:
+                    font = skia.Font(None, font_size)
+                blob = skia.TextBlob(i, font)
+                self.canvas.drawTextBlob(blob, offset, y, paint)
+                offset += font.measureText(i)
+
+    async def paste(self, image, position) -> None:
+        x, y = position
         img_height = image.dimensions().fHeight
         img_width = image.dimensions().fWidth
-        rec = skia.Rect.MakeXYWH(x,y,img_width,img_height)
-        canvas.drawImageRect(image,skia.Rect(0, 0,img_width, img_height),rec)
+        rec = skia.Rect.MakeXYWH(x, y, img_width, img_height)
+        self.canvas.drawImageRect(image, skia.Rect(
+            0, 0, img_width, img_height), rec)

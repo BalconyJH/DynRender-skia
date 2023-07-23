07cdcf3fb4ac6caa8100a9de462d8030ce506546
Polyisoprene <lzxder@outlook.com>
2023-07-23 11:58:11 +0800

    完善了所有动态类型的渲染

--- dynrender_skia/DynAdditional.py
+++ dynrender_skia/DynAdditional.py
@@ -1,13 +1,15 @@
-from .DynTools import DrawText, get_pictures, paste
+import re
+from abc import ABC, abstractmethod
+from os import path
+from typing import Optional
+
+import numpy as np
 import skia
 from dynamicadaptor.AddonCard import Additional
-from typing import Optional
-from .DynConfig import PolyStyle
-from abc import ABC, abstractmethod
-import numpy as np
-from os import path
 from loguru import logger
-import re
+
+from .DynConfig import PolyStyle
+from .DynTools import DrawText, get_pictures, paste
 
 
 class AbstractAdditional(ABC):
@@ -84,6 +86,10 @@ class BiliAdditional:
                 return await DynAddGoods(self.src_path, self.style, additional).run(repost)
             elif additional_type == "ADDITIONAL_TYPE_UGC":
                 return await DynAddUgc(self.src_path, self.style, additional).run(repost)
+            elif additional_type == "ADDITIONAL_TYPE_VOTE":
+                return await DynAddVote(self.src_path, self.style, additional).run(repost)
+            elif additional_type == "ADDITIONAL_TYPE_COMMON":
+                return await DynAddCommon(self.src_path, self.style, additional).run(repost)
             else:
                 logger.warning(f"{additional_type} IS NOT SUPPORT NOW")
                 return None
@@ -232,8 +238,11 @@ class DynAddUgc(AbstractAdditional):
 
     async def make_title_desc(self):
         draw = DrawText(self.style)
-        await draw.draw_text(self.canvas, self.additional.ugc.title, self.style.font.font_size.title, (430, 90, 990, 140, int(self.style.font.font_size.time*1.3)), self.style.color.font_color.text)
-        await draw.draw_text(self.canvas, self.additional.ugc.desc_second, self.style.font.font_size.title, (430, 200, 950, 200, 0), self.style.color.font_color.sub_title)
+        await draw.draw_text(self.canvas, self.additional.ugc.title, self.style.font.font_size.title,
+                             (430, 90, 990, 140, int(self.style.font.font_size.time * 1.3)),
+                             self.style.color.font_color.text)
+        await draw.draw_text(self.canvas, self.additional.ugc.desc_second, self.style.font.font_size.title,
+                             (430, 220, 950, 220, 0), self.style.color.font_color.sub_title)
 
     async def make_sub_tag(self):
         self.text_font.setSize(self.style.font.font_size.sub_title)
@@ -245,5 +254,122 @@ class DynAddUgc(AbstractAdditional):
         paint = skia.Paint(AntiAlias=True, Color=skia.Color4f.kWhite)
         canvas.drawTextBlob(blob, 10, int(self.text_font.getSize() + 5), paint)
         sub_tag_img = skia.Image.fromarray(canvas.toarray(colorType=skia.ColorType.kRGBA_8888_ColorType),
-                                             colorType=skia.ColorType.kRGBA_8888_ColorType)
-        await paste(self.canvas, await self.make_round_cornor(sub_tag_img, 10), (400-sub_tag_img.width()-15, 190))
+                                           colorType=skia.ColorType.kRGBA_8888_ColorType)
+        await paste(self.canvas, await self.make_round_cornor(sub_tag_img, 10), (400 - sub_tag_img.width() - 15, 190))
+
+
+class DynAddVote(AbstractAdditional):
+
+    async def run(self, repost) -> Optional[np.ndarray]:
+        background_color = self.style.color.background.repost if repost else self.style.color.background.normal
+        surface = skia.Surface(1080, 280)
+        self.canvas = surface.getCanvas()
+        self.canvas.clear(skia.Color(*background_color))
+        try:
+            await self.draw_shadow(self.canvas, (35, 20, 1010, 240), 15, background_color)
+            rec = skia.Rect.MakeXYWH(35, 20, 1010, 240)
+            self.canvas.clipRRect(skia.RRect(rec, 20, 20), skia.ClipOp.kIntersect)
+            await self.make_cover()
+            await self.make_title_desc()
+            await self.make_badge("投票", self.style.font.font_size.time, (860, 95), (155, 75), (42, 50))
+
+            return self.canvas.toarray(colorType=skia.ColorType.kRGBA_8888_ColorType)
+        except Exception as e:
+            logger.exception(e)
+            return None
+
+    async def make_cover(self):
+        cover = skia.Image.open(fp=path.join(self.src_path, "vote_icon.png")).resize(195, 195)
+        await paste(self.canvas, await self.make_round_cornor(cover, 10), (60, 45))
+
+    async def make_title_desc(self):
+        draw = DrawText(self.style)
+        await draw.draw_text(self.canvas, self.additional.vote.desc, self.style.font.font_size.text,
+                             (280, 110, 810, 110, 0),
+                             self.style.color.font_color.text)
+        if self.additional.vote.join_num is None:
+            join_num = "0人参与"
+        else:
+            join_num = f"{self.additional.vote.join_num}人参与"
+
+        await draw.draw_text(self.canvas, join_num, self.style.font.font_size.time,
+                             (280, 190, 810, 190, 0), self.style.color.font_color.sub_title)
+
+
+class DynAddCommon(AbstractAdditional):
+
+    async def run(self, repost) -> Optional[np.ndarray]:
+        background_color = self.style.color.background.repost if repost else self.style.color.background.normal
+        surface = skia.Surface(1080, 340)
+        self.canvas = surface.getCanvas()
+        self.canvas.clear(skia.Color(*background_color))
+        try:
+            await self.draw_shadow(self.canvas, (35, 80, 1010, 245), 15, background_color)
+
+            await DrawText(self.style).draw_text(self.canvas, self.additional.common.head_text,
+                                                 self.style.font.font_size.title, (50, 50, 1010, 90, 0),
+                                                 self.style.color.font_color.sub_title)
+
+            rec = skia.Rect.MakeXYWH(35, 80, 1010, 240)
+            self.canvas.clipRRect(skia.RRect(rec, 20, 20), skia.ClipOp.kIntersect)
+
+            await self.make_cover()
+            await self.select_badge()
+            await self.select_badge()
+            await self.make_title()
+            await self.make_desc()
+
+            return self.canvas.toarray(colorType=skia.ColorType.kRGBA_8888_ColorType)
+        except Exception as e:
+            logger.exception(e)
+            return None
+
+    async def make_cover(self):
+        if self.additional.common.sub_type in {'decoration', 'game'}:
+            cover_url = f"{self.additional.common.cover}@190w_190h_1c.webp"
+            cover = await get_pictures(cover_url, (190, 190))
+        else:
+            cover_url = f"{self.additional.common.cover}@145w_195h_1c.webp"
+            cover = await get_pictures(cover_url, (145, 195))
+        await paste(self.canvas, await self.make_round_cornor(cover, 15), (60, 110))
+
+    async def make_title(self):
+        if self.additional.common.desc2:
+            y = 150
+        else:
+            y = 180
+        if self.additional.common.sub_type in {'decoration', 'game'}:
+            x = 280
+        else:
+            x = 250
+        await DrawText(self.style).draw_text(self.canvas, self.additional.common.title, self.style.font.font_size.text,
+                                             (x, y, 780, y, 0), self.style.color.font_color.text)
+
+    async def make_desc(self):
+        draw = DrawText(self.style)
+        if self.additional.common.sub_type in {'decoration', 'game'}:
+            x = 280
+        else:
+            x = 250
+        if self.additional.common.desc2:
+            await draw.draw_text(self.canvas, self.additional.common.desc1, self.style.font.font_size.title,
+                                 (x, 220, 780, 160, 0), self.style.color.font_color.sub_title)
+
+            await draw.draw_text(self.canvas, self.additional.common.desc2, self.style.font.font_size.title,
+                                 (x, 285, 780, 225, 0), self.style.color.font_color.sub_title)
+        else:
+            await draw.draw_text(self.canvas, self.additional.common.desc1, self.style.font.font_size.title,
+                                 (x, 250, 780, 190, 0), self.style.color.font_color.sub_title)
+
+    async def select_badge(self):
+        badge_text_map = {
+            "pugv": "去试看",
+            "ogv": "去看看",
+            "manga": "去追漫",
+            "decoration": "去看看",
+            "game": "进入"
+        }
+        badge_text = badge_text_map.get(self.additional.common.sub_type, "去看看")
+        size = self.text_font.measureText(badge_text)
+        x = int((155 - size) / 2)
+        await self.make_badge(badge_text, self.style.font.font_size.time, (860, 165), (155, 75), (x, 50))
